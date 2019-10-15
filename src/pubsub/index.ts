49b2c622f907e535c00b7cbb9332e5db146caa5e
Asher Foster <me@asherfoster.com>
2019-10-15 13:49:12 +1300

    Configured dynamically creating subscriptions
    *Hopefully* we can rely on hostnames being a good label

--- src/pubsub/index.ts
+++ src/pubsub/index.ts
@@ -1,11 +1,16 @@
-import {PubSub, Topic} from '@google-cloud/pubsub';
+import {PubSub, Subscription, Topic} from '@google-cloud/pubsub';
+import {hostname} from 'os';
+
+// Defaulting to random is weird, but probably the best option
+const HOSTNAME = hostname() || Math.random().toString(36).substr(2);
+const ENV = process.env.NODE_ENV || 'development';
 
 // Singleton time!
 let _pubsub: PubSub;
 
 // Probably (?) best to cache it, right?
 const topicCache: Map<string, Topic> = new Map();
-export function topic(name: string): Topic {
+function getTopic(name: string): Topic {
   if (!name) {
     throw new Error('A name must be specified for a topic.');
   }
@@ -20,6 +25,26 @@ export function topic(name: string): Topic {
   }
 }
 
+export async function getSubscription(topicName: string, subName?: string): Promise<Subscription> {
+  subName = subName || topicName + '-' + ENV + '-' + HOSTNAME;
+  try {
+    const sub = _pubsub.subscription(subName);
+    await sub.get();
+    return sub;
+  } catch (e) {
+    console.debug('Couldn\'t find existing subscription, creating one:', subName);
+    const topic = getTopic(topicName);
+    const [sub, ] = await _pubsub.createSubscription(topic, subName, {
+      messageRetentionDuration: 10 * 60,
+      ackDeadlineSeconds: 10,
+      expirationPolicy: {
+        ttl: {seconds: 24 * 60 * 60}
+      }
+    });
+    return sub;
+  }
+}
+
 export function setup(opts?: any) {
   _pubsub = new PubSub(opts);
 }
@@ -31,6 +56,7 @@ export const scriptUpdateType = {
 
 export const pubsub = {
   setup,
-  topic,
+  getSubscription,
+  topic: getTopic,
   scriptUpdateType
 };
