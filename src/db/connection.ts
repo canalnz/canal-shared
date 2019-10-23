1b628edfcc345d81359e45ac33b4ed40a0f56171
Asher Foster <me@asherfoster.com>
2019-10-24 12:58:52 +1300

    Fix TIMESTAMP parsing to force no timezone to be UTC

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,4 +1,5 @@
 import {Connection, createConnection} from 'typeorm';
+import * as pg from 'pg';
 import * as path from 'path';
 
 export interface DatabaseConnectionOptions {
@@ -16,6 +17,9 @@ export async function createDbConnection({
   password,
   port = 5432
 }: DatabaseConnectionOptions): Promise<Connection> {
+  // Fix timezone parser to always parse dates as UTC. All dates in DB should be stored as utc
+  // See https://github.com/typeorm/typeorm/issues/2622#issuecomment-476416712
+  pg.types.setTypeParser(pg.types.TypeId.TIMESTAMP, (str: string) => new Date(`${str}+0000`));
   return await createConnection({
     type: 'postgres',
     database,
